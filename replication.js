fd98d2effc63a7a95bbb4450648f62f714c186db
akiro <akiro@lohika.com>
2020-08-21 11:07:00 +0300

    Replication

--- replication.js
+++ replication.js
@@ -21,6 +21,21 @@ async function init() {
     ]);
 }
 
+// async function testCRUD() {
+//     const db = nano.use(DB_NAME);
+//     const insertResult = await db.insert({testField: 'test1'});
+//     console.log('insertResult', insertResult);
+//     const getResult = await db.get(insertResult.id);
+//     console.log('getResult', getResult);
+//     const updateResult = await db.insert({_id: insertResult.id, _rev: insertResult.rev, testField: 'test2'});
+//     console.log('updateResult', updateResult);
+//     const getResultAfterUpdate = await db.get(insertResult.id);
+//     console.log('getResultAfterUpdate', getResultAfterUpdate);
+//     const destroyResult = await db.destroy(getResultAfterUpdate._id, getResultAfterUpdate._rev);
+//     console.log('destroyResult', destroyResult);
+//     const fetchRevsResult = await db.fetchRevs({keys: [getResultAfterUpdate._id]});
+//     console.log('fetchRevsResult', util.inspect(fetchRevsResult, false, null));
+// }
 async function test() {
     await init();
     await replicate();
@@ -34,9 +49,22 @@ async function createIfNotExists(client, dbs, dbName) {
 test();
 
 async function replicate() {
-    const db = nano.use(DB_NAME);
-    const insertResult = await db.insert({testField: 'test1'});
+    const db1 = nano.use(DB_NAME);
+    const db2 = nano2.use(DB2_NAME);
+    const insertResult = await db1.insert({testField: 'test1'});
     console.log('insertResult', insertResult);
-    const replicationResult = await nano2.db.replicate(DB_NAME, `${COUCHDB2_URL}/${DB2_NAME}`, {create_target: true});
+    const replicationResult = await nano.db.replicate(DB_NAME, `${COUCHDB2_URL}/${DB2_NAME}`, {create_target: true});
     console.log('replicationResult', replicationResult);
+
+    const updateResult2 = await db2.insert({_id: insertResult.id, _rev: insertResult.rev, testField: 'test3'});
+    console.log('updateResult db2', updateResult2);
+
+    const updateResult1 = await db1.insert({_id: insertResult.id, _rev: insertResult.rev, testField: 'test2'});
+    console.log('updateResult db1', updateResult1);
+
+    const replicationResult2 = await nano.db.replicate(DB_NAME, `${COUCHDB2_URL}/${DB2_NAME}`, {create_target: true});
+    console.log('replicationResult db => db2', replicationResult2);
+
+    const getResultAfterUpdate = await db2.get(insertResult.id);
+    console.log('getResultAfterReplicate db2', getResultAfterUpdate);
 }
